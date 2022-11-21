676452179b84c49e654067714cc035668a4bfd1a
KVNeykov18 <KVNeykov18@codingburgas.bg>
2022-11-21 11:41:08 +0200

    a

---
+++ src/main/java/com/example/qgoda/QgodaApplication.java
@@ -0,0 +1,13 @@
+package com.example.qgoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QgodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QgodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qgoda/QgodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qgoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QgodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
